f6ceb796b9a319a7d52c41a9af4b9e89ba4cf8d6
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-02-20 03:04:26 +0530

    fd

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:myportfolio/views/page1.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,7 +13,18 @@ class _HomePageState extends State<HomePage> {
     initialPage: 0,
     keepPage: true,
   );
+
   int _currentIndex = 0;
+  Color _menuIconColor = Colors.white;
+  Color _emailTextColor = Colors.white;
+  Color _phoneTextColor = Colors.white;
+  Color _copyrightTextColor = Colors.white;
+  Color _twitterIconColor = Colors.white;
+  Color _facebookIconColor = Colors.white;
+  Color _instagramIconColor = Colors.white;
+  Color _linkedInIconColor = Colors.white;
+  Color _githubIconColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,18 +33,21 @@ class _HomePageState extends State<HomePage> {
           PageView(
             controller: _pageViewController,
             scrollDirection: Axis.vertical,
+            allowImplicitScrolling: true,
             children: [
-              page1(context),
-              page1(context),
-              page1(context),
-              page1(context),
-              page1(context),
+              Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
+              // Page1(),
             ],
-            onPageChanged: (page) {
-              _pageViewController.animateToPage(page,
-                  duration: Duration(seconds: 1), curve: Curves.fastOutSlowIn);
+            onPageChanged: (index) {
               setState(() {
-                _currentIndex = page;
+                _currentIndex = index;
               });
             },
           ),
@@ -39,12 +55,214 @@ class _HomePageState extends State<HomePage> {
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    carouselDots(context),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0, vertical: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            "assets/logo.png",
+                            height: 45,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width > 900
+                                ? 150
+                                : 50,
+                          ),
+                          MediaQuery.of(context).size.width > 900
+                              ? InkWell(
+                                  child: Text(
+                                    "majhisambit2@gmail.com",
+                                    style: GoogleFonts.montserrat(
+                                      textStyle: TextStyle(
+                                        color: _emailTextColor,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                  onHover: (value) {
+                                    setState(() {
+                                      _emailTextColor = value
+                                          ? Colors.blueAccent
+                                          : Colors.white;
+                                    });
+                                  },
+                                  onTap: () {},
+                                )
+                              : Container(),
+                          MediaQuery.of(context).size.width > 900
+                              ? Text(
+                                  "  /  ",
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                )
+                              : Container(),
+                          MediaQuery.of(context).size.width > 900
+                              ? InkWell(
+                                  child: Text(
+                                    "+91 7751992236",
+                                    style: GoogleFonts.montserrat(
+                                      textStyle: TextStyle(
+                                        color: _phoneTextColor,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                  onHover: (value) {
+                                    setState(() {
+                                      _phoneTextColor = value
+                                          ? Colors.blueAccent
+                                          : Colors.white;
+                                    });
+                                  },
+                                  onTap: () {},
+                                )
+                              : Container(),
+                        ],
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.menu,
+                          size: 42,
+                          color: _menuIconColor,
+                        ),
+                        onHover: (value) {
+                          setState(() {
+                            _menuIconColor =
+                                value ? Colors.blueAccent : Colors.white;
+                          });
+                        },
+                        onTap: () {
+                          print(
+                            MediaQuery.of(context).size.width,
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.end,
+                //   children: [
+                //     Container(
+                //       padding: EdgeInsets.only(right: 16),
+                //       child: carouselDots(context),
+                //       width: 40,
+                //     ),
+                //   ],
+                // ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0, vertical: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        child: Text(
+                          "© Sambitraze 2021.",
+                          style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                              color: _copyrightTextColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        onHover: (value) {
+                          setState(() {
+                            _copyrightTextColor =
+                                value ? Colors.blueAccent : Colors.white;
+                          });
+                        },
+                        onTap: () {},
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            child: FaIcon(
+                              FontAwesomeIcons.twitter,
+                              color: _twitterIconColor,
+                            ),
+                            onHover: (value) {
+                              setState(() {
+                                _twitterIconColor =
+                                    value ? Colors.lightBlue : Colors.white;
+                              });
+                            },
+                            onTap: () {},
+                          ),
+                          SizedBox(width: 30,),
+                          InkWell(
+                            child: FaIcon(
+                              FontAwesomeIcons.instagram,
+                              color: _instagramIconColor,
+                            ),
+                            onHover: (value) {
+                              setState(() {
+                                _instagramIconColor =
+                                    value ? Colors.redAccent : Colors.white;
+                              });
+                            },
+                            onTap: () {},
+                          ),
+                          SizedBox(width: 30,),
+                          InkWell(
+                            child: FaIcon(
+                              FontAwesomeIcons.linkedin,
+                              color: _linkedInIconColor,
+                            ),
+                            onHover: (value) {
+                              setState(() {
+                                _linkedInIconColor =
+                                    value ? Colors.indigoAccent : Colors.white;
+                              });
+                            },
+                            onTap: () {},
+                          ),
+                          SizedBox(width: 30,),
+                          InkWell(
+                            child: FaIcon(
+                              FontAwesomeIcons.facebook,
+                              color: _facebookIconColor,
+                            ),
+                            onHover: (value) {
+                              setState(() {
+                                _facebookIconColor =
+                                    value ? Colors.blue[700] : Colors.white;
+                              });
+                            },
+                            onTap: () {},
+                          ),
+                          SizedBox(width: 30,),
+                          InkWell(
+                            child: FaIcon(
+                              FontAwesomeIcons.github,
+                              color: _githubIconColor,
+                            ),
+                            onHover: (value) {
+                              setState(() {
+                                _githubIconColor =
+                                    value ? Colors.green : Colors.white;
+                              });
+                            },
+                            onTap: () {},
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -56,18 +274,29 @@ class _HomePageState extends State<HomePage> {
 
   Widget carouselDots(context) {
     return ListView.builder(
+      shrinkWrap: true,
       itemBuilder: (context, index) {
-        return Container(
-          width: 10.0,
-          height: 10.0,
-          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: _currentIndex != index ? Colors.grey : Colors.blueAccent,
+        return InkWell(
+          onTap: () {
+            setState(() {
+              _currentIndex = index;
+            });
+            _pageViewController.animateToPage(_currentIndex,
+                duration: Duration(milliseconds: 800),
+                curve: Curves.fastOutSlowIn);
+          },
+          child: Container(
+            width: _currentIndex == index ? 18.0 : 10.0,
+            height: _currentIndex == index ? 18.0 : 10.0,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _currentIndex != index ? Colors.white : Colors.blueAccent,
+            ),
           ),
         );
       },
-      itemCount: 5,
+      itemCount: 9,
     );
   }
 }
--- lib/views/page1.dart
+++ lib/views/page1.dart
@@ -1,65 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Widget page1(context) {
-  return Container(
-    height: MediaQuery.of(context).size.height,
-    width: MediaQuery.of(context).size.width,
-    decoration: BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage("assets/bg1.png"),
-        fit: BoxFit.cover,
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  Color _playButtonColor = Colors.white;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/bg1.png"),
+          fit: BoxFit.cover,
+        ),
       ),
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          children: [
-            Flexible(
-              child: Container(),
-              flex: 1,
-            ),
-            Text.rich(
-              TextSpan(
-                text: 'Howdy,',
-                style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                    color: Colors.blueAccent,
-                        fontWeight: FontWeight.w500,
-                    fontSize: 72,
-                  ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(),
+                      flex: 1,
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text: 'Howdy,',
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 72,
+                          ),
+                        ),
+                        children: <InlineSpan>[
+                          MediaQuery.of(context).size.width < 800
+                              ? TextSpan(
+                                  text: "\nI'm\nSambit",
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 72,
+                                    ),
+                                  ),
+                                )
+                              : TextSpan(
+                                  text: "I'm\nSambit",
+                                  style: GoogleFonts.montserrat(
+                                    textStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 72,
+                                    ),
+                                  ),
+                                )
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(),
+                      flex: 3,
+                    ),
+                  ],
                 ),
-                children: <InlineSpan>[
-                  MediaQuery.of(context).size.width<800?TextSpan(
-                    text: "\nI'm\nSambit",
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 72,
-                      ),
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.20,
+                      child: Divider(
+                          height: 100, thickness: 1, color: Colors.white),
                     ),
-                  ):TextSpan(
-                    text: "I'm\nSambit",
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 72,
-                      ),
+                    SizedBox(
+                      width: 24,
                     ),
-                  )
-                ],
-              ),
+                    InkWell(
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundColor: _playButtonColor,
+                        child: Container(
+                          height: 75,
+                          width: 75,
+                          decoration: BoxDecoration(
+                            color: Color(0xff1C1C1C),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Icon(
+                            Icons.play_arrow_rounded,
+                            size: 32,
+                            color: _playButtonColor,
+                          ),
+                        ),
+                      ),
+                      onHover: (value) {
+                        setState(() {
+                          _playButtonColor =
+                              value ? Colors.blueAccent : Colors.white;
+                        });
+                      },
+                      onTap: () {},
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    InkWell(
+                      child: Text(
+                        "Watch Resume",
+                        style: GoogleFonts.montserrat(
+                          textStyle: TextStyle(
+                            color: _playButtonColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      onHover: (value) {
+                        setState(() {
+                          _playButtonColor =
+                              value ? Colors.blueAccent : Colors.white;
+                        });
+                      },
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Flexible(
-              child: Container(),
-              flex: 3,
-            ),
-          ],
-        )
-      ],
-    ),
-  );
+          ),
+        ],
+      ),
+    );
+  }
 }
